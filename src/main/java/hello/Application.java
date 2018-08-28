f6e27d6712fe3257b880445d5829921a6fc3af60
Boris Aranovič <b.aranovich@f5.com>
2018-08-28 15:09:42 +0300

    Initial commit for spring boot v1.5

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,12 +1,19 @@
 package hello;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(value = {ManyBeans.class})
 public class Application {
+    private static final Log logger = LogFactory.getLog(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        logger.info("Application loaded, exiting now...");
+        System.exit(0);
     }
 }
